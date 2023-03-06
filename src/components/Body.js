abb30b336414d563d868b7438680dd18a6184009
aneel.grewal <aneel.grewal@hotmail.com>
2023-03-06 19:14:51 +0100

    initial project setup with all packages

---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+
+
+
+const Body = () => {
+  return (
+    <div>
+        <h1 className="bg-green-200 min-h-screen">Body</h1>
+    </div>
+  )
+}
+
+export default Body;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+
+
+
+
+const Header = () => {
+  return (
+    <div className="">
+        <ul className="m-5 flex justify-center">
+            <li className="w-20 mx-1 bg-slate-500 flex justify-center font-bold text-yellow-50">Home</li>
+            <li className="w-20 mx-1 bg-slate-500 flex justify-center font-bold text-yellow-50">About</li>
+            <li className="w-20 mx-1 bg-slate-500 flex justify-center font-bold text-yellow-50">Contact</li>
+        </ul>
+    
+    </div>
+  );
+}
+
+export default Header;
