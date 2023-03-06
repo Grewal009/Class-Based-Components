c9deae4d9b03cff30e3e75726b6e4bfee200adfb
aneel.grewal <aneel.grewal@hotmail.com>
2023-03-06 23:45:02 +0100

    lifecycle methods of class based components

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,46 @@
+import React from "react";
+import Profile from "./Profile";
 
 
 
-
-const About = () => {
+const About1 = () => {
   return (
     <div className="bg-green-200 min-h-screen">About</div>
   )
+}
+
+class About extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      name : "React",
+      topic : "class based component",
+    }
+    console.log("About Constructor");
+  }
+
+  componentDidMount(){
+    console.log("About componentDidMount");
+
+  }
+
+  componentDidUpdate(){
+    console.log("About componentDidUpdate");
+  }
+
+
+  render(){
+    console.log("About render");
+
+    return(
+      <div className="bg-green-200 min-h-screen">
+        <h1>About page</h1>
+        <Profile name={this.state.name}/>
+       
+      </div>
+    );
+  }
 }
 
 export default About;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,41 @@
 
+import React from 'react';
 
 
-
-const Profile = () => {
+const Profile1 = () => {
   return (
     <div>Profile</div>
   )
+}
+
+class Profile extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("Profile Constructor");
+  }
+
+  componentDidMount(){
+    console.log("Profile componentDidMount");
+    this.timer = setInterval(()=>{console.log("Profile component")},1000);
+  }
+
+  componentDidUpdate(){
+    console.log("Profile componentDidUpdate");
+  }
+
+  componentWillUnmount(){
+    console.log("Profile componentWillUnmount");
+    clearInterval(this.timer);
+  }
+
+  render(){
+    console.log("Profile render");
+    return(
+      <div>
+        <h1>Profile component</h1>
+      </div>
+    );
+  }
 }
 
 export default Profile;
