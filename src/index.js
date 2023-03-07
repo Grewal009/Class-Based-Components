a8bc3c896cece244dea05b44f2dcb86595aa121d
aneel.grewal <aneel.grewal@hotmail.com>
2023-03-07 19:00:45 +0100

    Chunking/Lazy loading/Dynamic loading/On demand loading of About and Contact components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Error from "./components/Error";
-import About from "./components/About";
-import Contact from "./components/Contact";
+
 
 
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
+const About = lazy(()=>import("./components/About"));
+const Contact = lazy(()=>import("./components/Contact"));
 
 const AppLayout = () => {
     return(
@@ -30,11 +31,11 @@ const appRoute = createBrowserRouter([
          },
             {
             path : "/about",
-            element : <About />,
+            element : <Suspense fallback={<h1>loading...</h1>}><About /></Suspense>,
          },
          {
             path : "/contact",
-            element : <Contact />,
+            element : <Suspense fallback={<h1>loading component...</h1>}><Contact /></Suspense>,
          }
          ]
     },
