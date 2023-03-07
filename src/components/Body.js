d57a2debca5770bf771787069b96dcb71d3c2dc7
aneel.grewal <aneel.grewal@hotmail.com>
2023-03-07 11:09:49 +0100

    custom hook created i.e useOnline - to check internet connection

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,14 @@
 
-
+import useOnline from "../utils/useOnline";
 
 const Body = () => {
+
+  const isOnline = useOnline();
+  
+  if(!isOnline){
+    return <h1>Not online</h1>;
+  }
+
   return (
     <div>
         <h1 className="bg-green-200 min-h-screen">Body</h1>
