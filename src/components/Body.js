9c9c929ba96894d8c79d67e366d7d8a0dc09846a
aneel.grewal <aneel.grewal@hotmail.com>
2023-03-08 18:54:03 +0100

    use of dynamic classes in tailwindcss i.e []

--- src/components/Body.js
+++ src/components/Body.js
@@ -11,7 +11,11 @@ const Body = () => {
 
   return (
     <div>
-        <h1 className="bg-green-200 min-h-screen">Body</h1>
+        <div className="bg-green-200 min-h-screen">Body
+        <div className="m-2 w-28 bg-orange-400">box-1 (112px)</div>
+        <div className="m-2 w-[120] bg-orange-400">box-2 (120px)</div>
+        <div className= "m-2 w-32 bg-orange-400">box-2 (128px)</div>
+        </div>
     </div>
   )
 }
